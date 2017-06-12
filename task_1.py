328a0aab1f208549e189024bbcd4f0d175f62044
Anthony Lawrence <artisan@anthonylawrence.me.uk>
2017-06-12 21:32:49 +0100

    Version 2 - validation of user input.

--- task_1.py
+++ task_1.py
@@ -1,25 +1,35 @@
-# Troublshooting - Version 1
-# - Series of troubleshooting questions asked
+# Troublshooting - Version 2
+# - Each question to accept either YES or NO answer
+
+# This function will request user input and validate it's YES or NO.
+# This function will then return TRUE or FALSE respectively.
+def inputYN():
+    while True:
+        answer = input("YES or NO: >> ").upper()
+
+        if answer == "YES":
+            return True
+        elif answer == "NO":
+            return False
+
+    return None
 
 print("Welcome to the generic trouble shooter for a mobile phone.")
 
 print("Does your phone have signal?")
-answer = input(">> ")
-if answer.upper() == "YES":
+if inputYN():
     print("Your SIM card must be inserted.")
     input()
     exit(0)
     
 print("Have you dropped your phone?")
-answer = input(">> ")
-if answer.upper() == "YES":
+if inputYN():
     print("What a muppet.  You'll need to get that fixed.")
     input()
     exit(0)
 
 print("Has your phone suffered water damage?")
-answer = input(">> ")
-if answer.upper() == "YES":
+if inputYN():
     print("A jar of rice will fix that!")
     input()
     exit(0)
